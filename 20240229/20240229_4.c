e4406568ad0ad290115e8054b2dcff829158f30e
yckoincgu <eugene@chu.edu.tw>
2024-02-29 09:55:54 +0800

    20240229_5
    20240229_5

--- 20240229/20240229_4.c
+++ 20240229/20240229_4.c
@@ -6,11 +6,15 @@ static void catch_function(int signal) {
 }
 int main(void) {
 	if (signal(SIGINT, catch_function) == SIG_ERR) {
-		fputs("An error occurred while setting a signal handler.\n", stderr);
+		// When the user presses Ctrl+C, 
+		// the signalHandler (catch_function) function will be called
+		fputs("The Ctrl+C occurred with error under setting a signal handler.\n", stderr);
 		return EXIT_FAILURE;
 	}
 	puts("Raising the interactive attention signal.");
-	if (raise(SIGINT) != 0) {
+	if (raise(SIGINT) != 0) {	
+	// the raise function is used to send a signal to the current process.
+	// It is the interrupt signal, often generated when the user presses Ctrl+C in the terminal
 		fputs("Error raising the signal.\n", stderr);
 		return EXIT_FAILURE;
 	}
--- 20240229/20240229_5.c
+++ 20240229/20240229_5.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #include <setjmp.h>
-jmp_buf test1;
-void tryjump()
-{
-	longjmp(test1, 3);
+
+// Declare a global variable to hold the jump buffer
+jmp_buf jump_buffer;
+
+void functionWithJump() {
+    printf("Inside functionWithJump\n");
+
+    // Setjmp returns 0 when called directly, but returns a non-zero value
+    // when returning from longjmp
+    if (setjmp(jump_buffer) != 0) {
+        // This code is executed when longjmp is called
+        printf("Returning from longjmp\n");
+        return;
+    }
+
+    printf("About to call longjmp\n");
+
+    // Use longjmp to jump back to the point of setjmp
+    longjmp(jump_buffer, 1);
 }
-int main (void)
-{
-	if (setjmp(test1)==0) {
-		printf ("setjmp() returned 0.");
-		tryjump();} 
-	else {
-		printf ("setjmp returned from a longjmp function call.");
-	}
+
+int main() {
+    printf("Before calling functionWithJump\n");
+
+    // Call the function with setjmp
+    functionWithJump();
+
+    printf("After calling functionWithJump\n");
+
+    return 0;
 }
+
