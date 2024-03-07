23ec52cf4fa469685697ae9b44f1d7ec7e1c3fd0
yckoincgu <eugene@chu.edu.tw>
2024-03-07 11:21:19 +0800

    Update 20240229_block.c
    20240307_2

--- 20240229/20240229_block.c
+++ 20240229/20240229_block.c
@@ -1,10 +1,23 @@
+#include <stdio.h> 	/* for fprintf and stderr */
+
 int main(void)
 {
-	/* this is a 'block' */
-	int i = 5;
+	/* this is a 'C block' */
+	int i = 5;	// black i
 	{
-		/* this is also a 'block', nested inside the outer block */
-		int i = 6;
+		/* this is also a 'block', 
+		 1. containing executable statements
+		 2. the scope of declaration
+		    ex. a variable declared in this block
+			    its activity is limited in this block
+				outside this block, the variable is NOT available 
+		 3. block can be nested, i.e. there are blocks inside the outer block 
+		
+		*/
+		printf("before inner i is declared the black i = %d\n", i);
+		int i = 6;	// red i
+		printf("After inner i is declared the red i = %d\n", i);
 	}
+	printf("outsider i = %d\n", i);
 	return 0;
 }
