af8e9059f257fd18fe4bacbb21cb83e12d10935c
柯宇謙 <35418023+yckoincgu@users.noreply.github.com>
2024-03-28 00:17:50 +0800

    20240328_2
    20240328_2

---
+++ 20240328/pointer_6_function.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+	
+int primaryArithmetic(int *yarray);
+
+int main(void)
+{
+	int x=124, y=658;
+
+	printf("%d", primaryArithmetic(int x, int y));
+	
+	return 0;
+}
+
+int primaryArithmetic(int a, int b)
+{
+	int r=0, q=0;
+	int length;
+	while
+	return 0;
+}
+
