0a7480a980803d900650d3d5e3ead6e415de44d9
yckoincgu <eugene@chu.edu.tw>
2024-03-27 16:20:34 +0800

    20240327_3
    20240327_3

--- 20240328/pointer_1_array.c
+++ 20240328/pointer_1_array.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const int stringDelimiter=3;
+
 int main(void)
 {
 	char *ptr; 	// Declaration of an integer pointer
 				// 1. *  is the deferencing operator
-				// 2. value = *ptr;
-		  		// 3. In C, assigning an integer value directly to a pointer is generally not recommended
-	 			//    as it can lead to unexpected behavior and errors
+				// 2. ptr is an address, i.e. the pointed address
 	 			
-	char 	stringArray[]="abc";
+	char 	stringArray[]="abc";	// stringArray is the address of this array
 	
-	ptr = stringArray;
-	printf("the pointed character is %c \n", *ptr); // *ptr = 'a';   
-	printf("the stringArray is %s \n", stringArray); 
+	ptr = stringArray;	// stringArray is the address of an array
+	printf("the pointed character is %c \n", *ptr); // *ptr = 'a';
+	
+	printf("To print the pointed string, a loop is necessary for pointer \n");
+	int i;
+	for (i=0; i< sizeof(ptr)-stringDelimiter; i++) 
+		printf("%c", ptr[i]);	
+	printf("\n");  
+	  
+	printf("To print an array 'abc', the format \%\\s is necessary %s \n", stringArray); 
 	ptr++;
 	*ptr='x';		
 	printf("the stringArray after modification is %s \n", stringArray); 		
