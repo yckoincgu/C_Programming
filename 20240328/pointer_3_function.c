0709b1fd19ca7610430f7296ef6d36e2ab401fba
柯宇謙 <35418023+yckoincgu@users.noreply.github.com>
2024-03-27 20:58:47 +0800

    20240327_6
    20240327_6

---
+++ 20240328/pointer_3_function.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Student {
+	int id;
+	char *name;
+};
+	
+int assignStudentId(struct Student *studentAddress);
+
+int main(void)
+{
+	struct Student S1, S2, S3, S4;
+
+	S1.name="peter";
+	S2.name="john";
+	S3.name="mary";
+	S4.name="eric";
+	
+	printf("check Peter by function then assign id with ");
+	if(assignStudentId( &S2 ))  /* pointer returned from a function. */
+		printf("%d", S2.id);
+	else printf("This assignment failed.");	
+	return 0;
+}
+
+int assignStudentId(struct Student *studentAddress)
+{
+	int flag=1;
+	// strcmp: if equal then return 0
+	if (strcmp(studentAddress->name,"Peter")==0) studentAddress->id = 10;
+	else if (strcmp(studentAddress->name,"john")==0) studentAddress->id = 20;
+	else if (strcmp(studentAddress->name,"mary")==0) studentAddress->id = 30;
+	else if (strcmp(studentAddress->name,"eric")==0) studentAddress->id = 40;
+	else flag=0;
+	return flag;
+}
+
---
+++ 20240328/pointer_4_function.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+const int arrayDelimiter=3;
+	
+int getArrayLength(int xarray[]);
+
+int main(void)
+{
+	int S1[4]={0,1,2,3}, 
+		S2[5]={0,1,2,3,4}, 
+		S3[6]={0,1,2,3,4,5}, 
+		S4[7]={0,1,2,3,4,5,6};
+
+	printf("%d", getArrayLength( S2 ));
+	
+	return 0;
+}
+
+int getArrayLength(int arrayLength[])
+{
+	int i;
+	printf("check array length one by one: ");
+	for (i=0; i< sizeof(arrayLength)-arrayDelimiter; i++) 
+		printf("%d, ", arrayLength[i]);
+	printf("\n");
+	printf("The array length is ");
+	return (int)sizeof(arrayLength);
+}
+
