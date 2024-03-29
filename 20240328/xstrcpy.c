f6e8fa909c40ba5d3b52fdfb3fdbaa22eb0a1c98
yckoincgu <eugene@chu.edu.tw>
2024-03-29 13:09:28 +0800

    20240328_8
    20240328_8

--- 20240328/xstrcpy.c
+++ 20240328/xstrcpy.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
-
-char *(xstrcpy)(char *restrict s1, const char *restrict s2)
+/*
+	restrict: the object pointed to by that pointer will only be accessed 
+	through that pointer for the duration of its scope
+*/ 
+char *xstrcpy(char *s1, char *s2)
 {
-char *dst = s1;
-const char *src = s2;
-/* Do the copying in a loop. */
-while ((*dst++ = *src++) != '\0');
-/* The body of this loop is left empty. */
-/* Return the destination string. */
-return s1;
+	char *dst = s1;
+	const char *src = s2;
+	/* Do the copying in a loop. */
+	while ((*dst++ = *src++) != '\0');
+	*dst='\0';
+	
+	/* The body of this loop is left empty. */
+	/* Return the destination string. */
+	return s1;
 }
 
 
 int main(void)
 {
-	char s="test for xstrcpy";
-	char destinationStr=xstrcpy(destinationStr, s);
+	char s[]="test";
+	char x[sizeof(s)/sizeof(*s)];
 	
-	printf("%s", destinationStr);
 	
+	char *ptr=xstrcpy(x,s);
+	while (*ptr != '\0') printf("%c", *ptr++);
 }
