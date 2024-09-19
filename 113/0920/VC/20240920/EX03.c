3a8112228d618f6511ff35dc3617a5d6ebff713f
柯宇謙 <35418023+yckoincgu@users.noreply.github.com>
2024-09-19 15:44:25 +0800

    20240919_7
    20240919_7

---
+++ 113/0920/VC/20240920/EX03.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void EX01();
+void EX02();         // function prototype
+
+int main()
+{
+ 
+    EX01();
+    EX02();
+    return 0;
+}
+
+void EX01()         // function definition   
+{
+    printf("Hello World\n");
+}
+
+void EX02()
+{
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculate the sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d\n", number1, number2, sum);
+}
