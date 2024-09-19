b2c9940543ea4cbe1f63017ff05682c917128fc1
柯宇謙 <35418023+yckoincgu@users.noreply.github.com>
2024-09-19 22:27:35 +0800

    20240919_12
    20240919_12

--- 113/VC/0920/EX02.c
+++ 113/VC/0920/EX02.c
@@ -9,6 +9,6 @@ int main() {
     // calculate the sum
     sum = number1 + number2;      
     
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d + %d = %d\n", number1, number2, sum);
     return 0;
 }
