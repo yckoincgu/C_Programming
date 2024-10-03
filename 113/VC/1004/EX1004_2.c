0aacc260ccd1885937ae42013b976be82e96cf7c
柯宇謙 <35418023+yckoincgu@users.noreply.github.com>
2024-10-04 05:51:22 +0800

    20241004_5
    20241004_5

---
+++ 113/VC/1004/EX1004_2.c
@@ -0,0 +1,19 @@
+// Check whether an integer is odd or even
+// please change this program to execute EX01(), if input integer is odd;
+
+#include <stdio.h>
+int main() {
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    // True if the remainder is 0
+    if  (number%2 == 0) {
+        printf("%d is an even integer.",number);
+    }
+    else {
+        printf("%d is an odd integer.",number);
+    }
+
+    return 0;
+}
