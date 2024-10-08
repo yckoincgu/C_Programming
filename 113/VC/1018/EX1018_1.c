8d62cc7e3e677e1dc092def797e516039f336e0e
柯宇謙 <35418023+yckoincgu@users.noreply.github.com>
2024-10-08 08:05:52 +0800

    20241008_1
    20241008_1

---
+++ 113/VC/1018/EX1018_1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "lib_test.h"
+
+int main() {
+    testFunc_1();
+
+    return 0;
+}
---
+++ 113/VC/1018/lib_test.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void testFunc_1(){
+    printf("test libray \n");
+}
