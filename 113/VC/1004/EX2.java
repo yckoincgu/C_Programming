e3948e9794a254d62c13487cfa369f287445972a
柯宇謙 <35418023+yckoincgu@users.noreply.github.com>
2024-10-16 11:41:12 +0800

    20241016_1
    20241016_1

---
+++ 113/VC/1004/EX2.java
@@ -0,0 +1,20 @@
+package Ex01;
+
+public class EX2 {
+
+	    public static void main(String[] args) {
+	    	
+	    	int num1 = 9;
+	    	int num2 = 26;
+	    	int num3 = 14;
+	    	
+	    	if( num1 >= num2 && num1 >= num3)
+	            System.out.println(num1 + " is the largest number.");
+
+	        else if (num2 >= num1 && num2 >= num3)
+	            System.out.println(num2 + " is the largest number.");
+
+	        else
+	            System.out.println(num3 + " is the largest number.");
+	    }
+	}
