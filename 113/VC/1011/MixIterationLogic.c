de91963d92ade42b15533a21dec3f942e960df61
柯宇謙 <35418023+yckoincgu@users.noreply.github.com>
2025-02-12 11:13:21 +0800

    20250210_4
    20250210_4

---
+++ 113/VC/1011/MixIterationLogic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ()
+{
+	for(int i=0;i<=4;i++)
+	{	for(int j=4;j>=i;j--){ printf("x");}
+	    printf("\n");}
+	printf("\n");
+	for(int i=0;i<=4;i++)
+	{	for(int j=0;j<=4;j++)
+		{   if(j>=i){printf("x");}
+			else{printf(" ");}}
+	    printf("\n");
+	}
+	
+	
+	
+	return 0;
+}
+ 
